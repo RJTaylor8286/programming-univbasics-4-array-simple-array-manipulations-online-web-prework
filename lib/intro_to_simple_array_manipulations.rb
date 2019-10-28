12a7dd38a047547872c85417dea1f327c61e7a73
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-10-28 14:49:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,9 @@
 artists = ["Hozier", "Ariana Grande", "Usher"]
 next_artist = "Beyonce"
 artists.push(next_artist)
+p
 #=> ["Hozier", "Ariana Grande", "Usher", "Beyonce"]
 
 artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
 artists.pop
+p
